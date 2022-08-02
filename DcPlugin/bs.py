b5ef7209f4ef31f9e4c77cbe6253dc38eecfe00e
ShaKaOp <89745675+CallMeShaKa@users.noreply.github.com>
2022-08-02 18:07:47 +0530

    Create bs.py

---
+++ DcPlugin/bs.py
@@ -0,0 +1,18 @@
+#
+# Copyright (C) 2022-2023 by DeCode@Github, < https://github.com/TeamDeCode >.
+#
+# This file is part of < https://github.com/TeamDeCode/DcSpamBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamDeCode/DcSpamBot/blob/main/LICENSE >
+#
+# All rights reserved.
+
+import os 
+
+START_TEXT = f"**Hey 👋**\n\n`Welcome To DcSpamBot`\n`This SpamBot Is Powerfull SpamBot Made On Python With PyroGram`\n\n`This Is Open Source SpamBot To Raid Chats In TeleGram Within Fight Between Two Clan Or Between To People`\n\n`Please Not That Anything Wrong Happeneds With This SpamBot To Anyone Then We Are Not Responsible Cause We Just Created The Source Not The Handler Of The Bot!`"
+HELP_TEXT = '**Hey 👋**\n\n`Welcome To Help Section Of DcSpamBot`\n`Below Are The Commands That Be Used By Sudo Users`\n`Click On Buttons To Get Help One By One`\n\n**Click Back To Move To Homepage**' 
+SPAM_TEXT = '''**Hey 👋**\n\n**Welcome To Spam Help Section Below Listed Are Commands Comes Under Spam Plugin Read All**\n\n**SPAM COMMANDS**\n\n⇏ `/spam`\n⇏ `/delayspam`\n⇏ `/fastspam`\n⇏ `/slowspam`\n⇏ `/stickerspam`\n⇏ `/pornspam`'''
+TOOLS_TEXT = "**Hey 👋**\n\n**Welcome To Tools Help Section Below Listed Are Commands Comes Under Spam Plugin Read All**\n\n**TOOLS COMMANS**\n\n⇏ `/leave`\n⇏ `/alive`\n⇏ `/ping`\n⇏ `/restart`"
+RAID_TEXT = '**Hey 👋**\n\n**Welcome To ReplyRaid Help Section Below Listed Are Commands Comes Under Spam Plugin Read All**\n\n**REPLY RAID COMMANDS**\n\n⇏ `/raid`'
+HANG_TEXT = "**Hey 👋**\n\n**Welcome To Hang Help Section Below Listed Are Commands Comes Under Spam Plugin Read All**\n\n**HANG COMMANDS**\n\n⇏ `/hang`"
+CARBON_TEXT = "**Hey 👋**\n\n**Welcome To Carbon Help Section Below Listed Are Commands Comes Under Spam Plugin Read All**\n\n**CARBON COMMANDS**\n\n⇏ `/karb`"
